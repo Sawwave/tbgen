472d8bed23ceeb4b084877ae59a571d273dc52b4
Tim Anderson <tim.anderson@umontana.edu>
2018-05-07 15:25:33 -0700

    Update tbgen.py

--- tbgen.py
+++ tbgen.py
@@ -5,6 +5,8 @@
 # can do whatever you want with this stuff. If we meet some day, and you think
 # this stuff is worth it, you can buy me a beer in return Xiongfei(Alex) Guo.
 
+#Modified May 2018, Tim Anderson
+
 '''
 Created on 2010-4-23
 
@@ -27,10 +29,12 @@ class TestbenchGenerator(object):
             self.ofile = sys.stdout
         self.vcont = ""
         self.mod_name = ""
+        self.mod_params = []
+        self.mod_ports = []
         self.pin_list = []
         self.clock_name = 'clk'
         self.reset_name = 'rst'
-        
+
         if vfile_name == None:
             sys.stderr.write("ERROR: You aren't specfic a input file name.\n")
             sys.exit(1)
@@ -42,21 +46,21 @@ class TestbenchGenerator(object):
     def open(self, vfile_name = None):
         if vfile_name != None:
             self.vfile_name = vfile_name
-            
+
         try:
             self.vfile = open(self.vfile_name, 'r')
-            self.vcont = self.vfile.read() 
+            self.vcont = self.vfile.read()
         except Exception, e:
             print "ERROR:Open and read file error.\n ERROR:    %s" % e
             sys.exit(1)
-            
+
     def open_outputfile(self, ofile_name = None):
         try:
             if(ofile_name == None):
                 if(self.ofile_name == None):
                     ofname = "tb_%s.v" % self.mod_name
                     self.ofile = open(ofname, 'w')
-                    print "Your are not specify a output file name, use '%s' instead." % ofname 
+                    print "Your are not specify a output file name, use '%s' instead." % ofname
                 else:
                     self.ofile = open(self.ofile_name, 'w')
                     print "Output file is '%s'." % self.ofile_name
@@ -66,7 +70,7 @@ class TestbenchGenerator(object):
         except Exception, e:
             print "ERROR:open and write output file error. \n ERROR:    %s" % e
             sys.exit(1)
-                
+
     def clean_other(self, cont):
         ## clean '// ...'
         cont = re.sub(r"//[^\n^\r]*", '\n', cont)
@@ -79,27 +83,36 @@ class TestbenchGenerator(object):
         ## clean '\n' * '\r'
         #cont = re.sub(r'[\n\r]+', '', cont)
         return cont
-        
+
     def parser(self):
         print "Parsering..."
-        # print vf_cont 
-        mod_pattern = r"module[\s]+(\S*)[\s]*\([^\)]*\)[\s\S]*"  
-        
+        # print vf_cont
+        module_regex = string(r"[\s]+module[\s](\S*)" //match module name
+        "[\s]*(#[\s]*\([^\)]*\))?"  //match module parameters
+        "([\s]*\([^;]*);")          //match module ports
         module_result = re.findall(mod_pattern, self.clean_other(self.vcont))
         #print module_result
         self.mod_name = module_result[0]
-        
+        self.mod_params = parse_mod_params(module_results[1])
+        self.mod_ports = parse_mod_ports(module_results[2])
+
         self.parser_inoutput()
         self.find_clk_rst()
-             
-    
-    def parser_inoutput(self):
-        pin_list = self.clean_other(self.vcont) 
+
+
+    def parse_mod_params(self, param_string):
+        raise NotImplementedError
         
+    def parse_mod_ports(self, port_string):
+        raise NotImplementedError
+
+    def parser_inoutput(self):
+        pin_list = self.clean_other(self.vcont)
+
         comp_pin_list_pre = []
         for i in re.findall(r'(input|output|inout)[\s]+([^;,\)]+)[\s]*[;,]', pin_list):
             comp_pin_list_pre.append((i[0], re.sub(r"^reg[\s]*", "", i[1])))
-            
+
         comp_pin_list = []
         type_name = ['reg', 'wire', 'wire', "ERROR"]
         for i in comp_pin_list_pre:
@@ -119,36 +132,36 @@ class TestbenchGenerator(object):
                 comp_pin_list.append((i[0], x[1], x[0] + ']', type_name[type]))
             else:
                 comp_pin_list.append((i[0], x[0], '', type_name[type]))
-        
+
         self.pin_list = comp_pin_list
         # for i in self.pin_list: print i
-        
+
     def print_dut(self):
         max_len = 0
         for cpin_name in self.pin_list:
             pin_name = cpin_name[1]
             if len(pin_name) > max_len:
                 max_len = len(pin_name)
-        
-        
+
+
         self.printo( "%s uut (\n" % self.mod_name )
-        
+
         align_cont = self.align_print(map(lambda x:("", "." + x[1], "(", x[1], '),'), self.pin_list), 4)
         align_cont = align_cont[:-2] + "\n"
         self.printo( align_cont )
-        
+
         self.printo( ");\n" )
-        
+
     def print_wires(self):
         self.printo(self.align_print(map(lambda x:(x[3], x[2], x[1], ';'), self.pin_list), 4))
         self.printo("\n")
-    
+
     def print_clock_gen(self):
         fsdb = "    $dumpfile(\"db_tb_%s.vcd\");\n    $dumpvars(0, tb_%s);\n" % (self.mod_name, self.mod_name)
 
         clock_gen_text = "\nparameter PERIOD = 10;\n\ninitial begin\n%s    CLK = 1'b0;\n    #(PERIOD/2);\n    forever\n        #(PERIOD/2) CLK = ~CLK;\nend\n\n" % fsdb
         self.printo(re.sub('CLK', self.clock_name, clock_gen_text))
-        
+
     def find_clk_rst(self):
         for pin in self.pin_list:
             if re.match(r'[\S]*(clk|clock)[\S]*', pin[1]):
@@ -164,7 +177,7 @@ class TestbenchGenerator(object):
 
     def print_module_head(self):
         self.printo("`include \"timescale.v\"\nmodule tb_%s;\n\n" % self.mod_name)
-        
+
     def print_module_end(self):
         self.printo("endmodule\n")
 
@@ -188,11 +201,11 @@ class TestbenchGenerator(object):
             for i in range(row_len):
                 l = len(col[i])
                 align_cont[i] += "%s%s" % (col[i], (indent + max_len - l) * ' ')
-        
+
         # remove space in line end
         align_cont = map(lambda s:re.sub('[ ]*$', '', s), align_cont)
         return "\n".join(align_cont) + "\n"
-        
+
 
 if __name__ == "__main__":
     print '''***************** tbgen - Auto generate a testbench. *****************
@@ -206,7 +219,7 @@ License: Beerware
         sys.exit(1)
     elif len(sys.argv) == 3:
         ofile_name = sys.argv[2]
-        
+
     tbg = TestbenchGenerator(sys.argv[1], ofile_name)
 
     tbg.print_module_head()
@@ -215,4 +228,3 @@ License: Beerware
     tbg.print_clock_gen()
     tbg.print_module_end()
     tbg.close()
-
